5981a2a472d76806a7574bb4fd9e9881a9fb6328
jose hernandez <jose2889@gmail.com>
2018-07-02 17:28:07 -0400

    instalacion de material completada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { QRCodeModule } from 'angular2-qrcode';
 import { AppComponent } from './app.component';
 import { CodigoqrComponent } from './components/codigoqr/codigoqr.component';
-import { FormsModule, ReactiveFormsModule, NgForm } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormulariosComponent } from './components/formularios/formularios.component';
 import { appRouting } from './app.routes';
 import { HomeComponent } from './components/home/home.component';
+import { MaterialModule } from './material';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 
 @NgModule({
@@ -15,7 +20,8 @@ import { HomeComponent } from './components/home/home.component';
     AppComponent,
     CodigoqrComponent,
     FormulariosComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
     
   ],
   imports: [
@@ -23,8 +29,9 @@ import { HomeComponent } from './components/home/home.component';
     QRCodeModule,
     FormsModule,
     appRouting, 
-    ReactiveFormsModule, 
-    NgForm
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MaterialModule
     
 
   ],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 
 import { RouterModule, Routes } from '@angular/router';
 import { FormulariosComponent } from './components/formularios/formularios.component';
-import { QRCodeComponent } from 'angular2-qrcode';
 import { HomeComponent } from './components/home/home.component';
+import { CodigoqrComponent } from './components/codigoqr/codigoqr.component';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'formulario', component: FormulariosComponent },
-    { path: 'qr', component: QRCodeComponent },
+    { path: 'qr', component: CodigoqrComponent },
     { path: '**', pathMatch:'full', redirectTo: 'home' }
 ];
 
