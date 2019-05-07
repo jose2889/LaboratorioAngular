a5b31a8212177be86dcf3de85c0c62ffbd8fce5b
JOSE HERNANDEZ <jhernandez@gruposolux.com>
2019-05-07 13:11:14 -0400

    convertir imagen en base 64

--- src/app/components/blag-tab/listar/listar.component.ts
+++ src/app/components/blag-tab/listar/listar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-listar',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar.component.css']
 })
 export class ListarComponent implements OnInit {
+  private base64textString:String="";
+  imagePath:any;
 
-  constructor() { }
+  constructor(private _sanitizer: DomSanitizer) { }
 
   ngOnInit() {
   }
+
+  handleFileSelect(evt){
+    var files = evt.target.files;
+    var file = files[0];
+  
+  if (files && file) {
+      var reader = new FileReader();
+
+      reader.onload =this._handleReaderLoaded.bind(this);
+
+      reader.readAsBinaryString(file);
+    }
+  }
+
+  _handleReaderLoaded(readerEvt) {
+    var binaryString = readerEvt.target.result;
+            this.base64textString= btoa(binaryString);
+            console.log(btoa(binaryString));
+            this.imagePath = this._sanitizer.bypassSecurityTrustResourceUrl('data:image/jpg;base64,'
+            + this.base64textString);
+    }
 
 }
